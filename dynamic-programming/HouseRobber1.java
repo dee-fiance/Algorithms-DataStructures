b5462739a817565d66b1d52b324b79a9881ce11a
Deepika Panda <deepika.destiny@gmail.com>
2019-01-07 23:26:23 -0800

    Update and rename houseRobber1.java to HouseRobber1.java

--- dynamic-programming/HouseRobber1.java
+++ dynamic-programming/HouseRobber1.java
@@ -11,17 +11,18 @@ The key is to find the relation dp[i] = Math.max(dp[i-1], dp[i-2]+ nums[i]).
 
 Time: O(n) 
 **/
-
-public int houseRobber1(int[] nums) {
-    if(nums==null||nums.length==0)
-        return 0;
-    if(nums.length==1)
-        return nums[0];
-    int[] dp = new int[nums.length];
-    dp[0]=nums[0];
-    dp[1]=Math.max(nums[0], nums[1]);
-    for(int i=2; i<nums.length; i++){
-      dp[i] = Math.max(dp[i-2]+nums[i], dp[i-1]); 
-    }
-    return dp[nums.length-1];
-} 
+public class HouseRobber1 {
+    public int houseRobber1(int[] nums) {
+        if(nums==null||nums.length==0)
+            return 0;
+        if(nums.length==1)
+            return nums[0];
+        int[] dp = new int[nums.length];
+        dp[0]=nums[0];
+        dp[1]=Math.max(nums[0], nums[1]);
+        for(int i=2; i<nums.length; i++){
+          dp[i] = Math.max(dp[i-2]+nums[i], dp[i-1]); 
+        }
+        return dp[nums.length-1];
+    } 
+}
